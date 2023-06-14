43f3b0e90c3aac335e441f3e820c9bdf69c13243
Giel Willemsen <33036522+GWillemsen@users.noreply.github.com>
2023-06-14 18:05:19 +0200

    Properly name test use case

--- test/test_userdata.cpp
+++ test/test_userdata.cpp
@@ -5,7 +5,7 @@
 #include "at_parser/at_parser.h"
 #include "parser_helpers.h"
 
-TEST_CASE("Test garbage commands")
+TEST_CASE("Test commands with userdata arguments")
 {
     at_parser_handle_t handle = nullptr;
     commands.clear();
@@ -28,25 +28,5 @@ TEST_CASE("Test garbage commands")
     CHECK_EQ((void*)0x0010, commands[1].userdata);
     CHECK_EQ(AT_PARSER_COMMAND_TYPE_SET, commands[1].type);
 
-    at_parser_free(handle);
-}
-
-TEST_CASE("Unkown AT Command")
-{
-    at_parser_handle_t handle = nullptr;
-    commands.clear();
-    CHECK_EQ(0, at_parser_create(&handle, 50, '\x1B', ','));
-    CHECK_EQ(0, at_parser_add_command_handler(handle, "HELLOW", at_parser_default_received_command, NULL));
-    CHECK_EQ(0, at_parser_add_command_handler(handle, "ABC", at_parser_default_received_command, NULL));
-    CHECK_EQ(0, at_parser_add_command_handler(handle, "DEF", at_parser_default_received_command, NULL));
-
-    const char *buffer = "AT+AD=\"hi\"\r\nAT+ABC=def\r\n";
-    CHECK_EQ(0, at_parser_process_buffer(handle, buffer, strlen(buffer)));
-    CHECK_EQ(1, commands.size());
-    CHECK(std::string("ABC") == commands[0].command);
-    CHECK_EQ(1, commands[0].arguments.size());
-    CHECK_EQ(std::string("def"), commands[0].arguments[0]);
-    CHECK_EQ(AT_PARSER_COMMAND_TYPE_SET, commands[0].type);
-    
     at_parser_free(handle);
 }
